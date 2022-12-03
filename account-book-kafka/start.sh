a2c0e0b6517b064ac7c2a4889b924b81ffe7b017
minseokim <mojave0102@gmail.com>
2022-12-03 19:37:28 +0900

    [3] Kafka & Zookeeper environment setup script

---
+++ account-book-kafka/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# command to create and run two component images
+docker-compose -f docker-compose.yml up -d
+# make sure ports are open
+nc -z localhost 22181
+nc -z localhost 29092
+
+docker exec -i -t account-book-kafka bash /bin/zookeeper-server-start -daemon
+docker exec -i -t account-book-kafka bash /bin/kafka-server-start -daemon
+
+SET_TOPIC=account-book-log.1
+CURRENT_TOPICS=`docker exec -i -t account-book-kafka bash /bin/kafka-topics --list --bootstrap-server=localhost:29092`
+
+if [[ $CURRENT_TOPICS == *$SET_TOPIC* ]]; then
+    echo "Topic already exists. no creation request."
+else
+    docker exec -i -t account-book-kafka bash /bin/kafka-topics --create --topic $SET_TOPIC --bootstrap-server=localhost:29092
+fi
